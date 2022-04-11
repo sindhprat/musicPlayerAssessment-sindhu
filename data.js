ff15d2a76f2c0c99dce5a2b09bbb01d4bf118ecd
sindhprat <99783829+sindhprat@users.noreply.github.com>
2022-04-11 10:29:22 +1000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name : 'Ukulele',
+        path: 'music/ukulele.mp3',
+        artist : 'Benjamin Tissot',
+        cover: 'images/cover1.jpg'
+    },
+    {
+        name : 'betterdays',
+        path: 'music/betterdays.mp3',
+        artist : 'Benjamin Tissot',
+        cover: 'images/cover2.jpg'
+    },
+    {
+        name : 'sunny',
+        path: 'music/sunny.mp3',
+        artist : 'Benjamin Tissot',
+        cover: 'images/cover3.jpg'
+    }
+]
